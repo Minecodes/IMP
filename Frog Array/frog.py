0d340d46f2035d13efe71f8e6eec36ed2a485da3
Minecodes <minecodes13@gmail.com>
2021-12-22 17:29:45 +0100

    * frog.py - Documentation added

--- Frog Array/frog.py
+++ Frog Array/frog.py
@@ -6,6 +6,7 @@ jump = 0
 longest = 0
 average = 0
 
+# Jump lengths for the frog
 for i in range(10):
     jump = random.randint(0,20)
     jumps.append(jump)
@@ -13,10 +14,12 @@ for i in range(10):
         longest = jump
     print("The frog jumped %d cm" % jump)
 
+# Calculate the average jump length
 for i in range(len(jumps)):
     average += jumps[i]
 average = math.floor(average / len(jumps))
 
+# Print the results
 print("The frog jumped", jumps)
 print("The longest jump was %d cm" % longest)
 print("The average jump was %d cm" % average)
